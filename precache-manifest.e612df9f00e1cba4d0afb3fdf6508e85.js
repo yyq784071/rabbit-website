b8dda3352d6719b6f03d57f46bf74429d117cc18
yyq <2410985372@qq.com>
2021-11-08 22:37:02 +0800

    deploy

---
+++ precache-manifest.e612df9f00e1cba4d0afb3fdf6508e85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8df35a463b069b14d0d",
+    "url": "/rabbit-website/css/app.40cadac7.css"
+  },
+  {
+    "revision": "2457231cf68c9b538a46e57d6e3b9af5",
+    "url": "/rabbit-website/index.html"
+  },
+  {
+    "revision": "a8df35a463b069b14d0d",
+    "url": "/rabbit-website/js/app.14e2ff32.js"
+  },
+  {
+    "revision": "982448a0b501c7fbed2a",
+    "url": "/rabbit-website/js/chunk-vendors.ffa0f724.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/rabbit-website/manifest.json"
+  },
+  {
+    "revision": "9b4abd019c155288297230066cff35ba",
+    "url": "/rabbit-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/rabbit-website/robots.txt"
+  }
+]);
